fd4f437344ce7198adc4cc0ca9cb40f8f21a87b9
André <113451004+andrehataishi@users.noreply.github.com>
2023-10-11 19:31:05 +0000

    Adicionando a possibilidade de definir os nomes dos candidatos antes da votação

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,6 +1,11 @@
 function urnaEletronica(){
     const contadorMenu = 0;
     let candidato1 = 0, candidato2 = 0, candidato3 = 0, votoBranco = 0, votoNulo = 0, votoTotal = 0, votoVálido = 0;
+    
+    console.log("Definindo os nomes dos candidatos antes da votação.")
+    let nome1 = prompt("Digite o nome do candidato 1: ").toUpperCase()
+    let nome2 = prompt("Digite o nome do candidato 2: ").toUpperCase()
+    let nome3 = prompt("Digite o nome do candidato 3: ").toUpperCase()
 
     while (contadorMenu===0) {
         console.log(`
@@ -8,9 +13,9 @@ function urnaEletronica(){
         
         Selecione uma opção:
 
-        (1) Votar cadidato 1
-        (2) Votar cadidato 2
-        (3) Votar cadidato 3
+        (1) Votar no candidato: ${nome1}
+        (2) Votar no candidato: ${nome2}
+        (3) Votar no candidato: ${nome3}
         (5) Votar em branco
         (8) Votar nulo
         (0) Encerrar votação`);
@@ -35,17 +40,17 @@ function urnaEletronica(){
             } else if (opcao===0){
                 console.clear()
                 console.log("Total de votos válidos:" , votoVálido = candidato1 + candidato2 + candidato3)
-                console.log("Total de votos candidado 1:", candidato1)
-                console.log("Total de votos candidado 2:", candidato2)
-                console.log("Total de votos candidado 3:", candidato3)
+                console.log(`Total de votos do candidato: ${nome1}:`, candidato1 )
+                console.log(`Total de votos do candidato: ${nome2}:`, candidato2)
+                console.log(`Total de votos do candidato: ${nome3}:`, candidato3)
                 console.log("Total de votos em branco:", votoBranco)
                 console.log("Total de votos nulo:", votoNulo)
                 if (candidato1 > candidato2 || candidato1 > candidato3){                    
-                    console.log("\nCom" , candidato1 + votoBranco ,"voto(s). O vencedor das eleições é o CANDIDATO 1!")
+                    console.log("\nCom" , candidato1 + votoBranco ,"voto(s). O vencedor das eleições é o candidato:" , nome1)
                 } else if (candidato2 > candidato1 || candidato2 > candidato3){
-                    console.log("\nCom" , candidato2 + votoBranco ,"voto(s). O vencedor das eleições é o CANDIDATO 2!")
+                    console.log("\nCom" , candidato2 + votoBranco ,"voto(s). O vencedor das eleições é o candidato:" , nome2)
                 } else if (candidato3 > candidato2 || candidato3 > candidato2){
-                    console.log("\nCom" , candidato3 + votoBranco ,"voto(s). O vencedor das eleições é o CANDIDATO 3!")
+                    console.log("\nCom" , candidato3 + votoBranco ,"voto(s). O vencedor das eleições é o candidato:" , nome3)
                 } else {
                     console.log("\nHouve empate nas eleições!")
                 }

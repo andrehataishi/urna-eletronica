cbd67dac53554736deb935f70a67000e7a8785d9
André <113451004+andrehataishi@users.noreply.github.com>
2023-11-06 20:28:53 +0000

    refactor: refatorando candidatos pré-configurados

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -2,132 +2,139 @@
  * SENAI-SP - Conde José Vicente de Azevedo 
  * Criação de uma urna eletrônica */
 
-function urnaEletronica(){
+
+let candidatos = [
+    [7, "Cristiano Ronaldo"],
+    [9, "Ronaldo Cascão"],
+    [10, "Lionel Messi"],
+    [11, "Neymar Jr."],
+    [18, "Ronaldinho Gaúcho"],
+    [99, "Para Voto em Branco"]];
+
+    document.getElementById("tabela").innerHTML +=
+        `<table>
+        <th>Numero</th>
+        <th>Nome</th>
+        </table>`
+    for (let i = 0; i < candidatos.length; i++) {
+        document.getElementById("tabela").innerHTML +=
+        `<table>
+        <tr>
+        <td>${candidatos[i][0]}</td>
+        <td>${candidatos[i][1]}</td>
+        </tr>
+        </table>`
+    }
+
+function urnaEletronica() {
 
     /* declaração das variáveis */
     const contadorMenu = 0;
-    let candidato1 = 0, candidato2 = 0, candidato3 = 0, votoBranco = 0, votoNulo = 0, votoTotal, votoVálido = 0, nome1, nome2, nome3;
+    let maiorVoto = 0, votoBranco, votoNulo = 0, votoTotal = 0, votoVálido = 0, opcaoEscolhida = false, ganhador,
 
-    /* construindo uma estrutura de repetição para o mesário inserir os nomes dos candidatos, podendo inserir dentro do prompt */
-    while (contadorMenu===0){
-        console.log("Definindo os nomes dos candidatos antes da votação.")
-        nome1 = prompt("Digite o nome do candidato 1: ").toUpperCase() /* deixando os nomes em caixa alta com toUpperCase */
-        nome2 = prompt("Digite o nome do candidato 2: ").toUpperCase()
-        nome3 = prompt("Digite o nome do candidato 3: ").toUpperCase()
-        
-        /* mostrando os nomes definidos pelo mesário, a fim de confirmar se estão corretos ou não e possibilitando a correção deles*/
-        let libera = confirm(`Nome do candidato 1: ${nome1}\nNome do candidato 2: ${nome2}\nNome do candidato 3: ${nome3}\n\nO nome dos candidatos está correto?\n\n(OK) para liberar a votação\n(CANCELAR) para reeditar os nomes dos candidatos`) /* com o método confirm */
+        //chamando a funcao para armazenar o horario do inicio da votação
+        horaInicio = dataUrna();
 
-        /* caso os nomes estiverem corretos e o mesário apertar a confimação a repetição é encerrada */
-        if (libera===true){
-            console.clear();  
-            
-            break;
-        }  
+    for (let i = 0; i < candidatos.length; i++) {
+        candidatos[i].push(0)
     }
 
-    //chamando a funcao para armazenar o horario do inicio da votação
-    horaInicio = dataUrna();
-
     /* menu da urna com estrutura de repetição */
-    while (contadorMenu===0) {
-
-        /* é mostrado as opções de voto para o usuário */
-        console.log(`
-        ***URNA ELETRONICA***
-        
-        Selecione uma opção:
-
-        (1) Votar no candidato: ${nome1}
-        (2) Votar no candidato: ${nome2}
-        (3) Votar no candidato: ${nome3}
-        (5) Votar em branco`);
+    while (contadorMenu === 0) {
 
         /* entrada da opção de voto */
-        let opcao = parseInt(prompt("Digite sua opção de voto: ")); 
+        let opcao = parseInt(prompt("Digite sua opção de voto: "));
+        console.clear()
 
-        /* limpando o console para evitar poluição visual */
-        console.clear() 
-        if (opcao===1){
-            /* confirmação da opção de voto, abrindo a possibilidade de correção caso o usuário tenha inserido uma opção errada */
-            let confOpc = confirm(`Sua opção de voto: ${opcao}\nCandidato: ${nome1}\n\nConfimar opção?\n\n(OK) para confimar voto\n(CANCELAR) para corrigir voto`) 
-            if (confOpc===true){  
-                candidato1 ++
-                console.log("Voto computado.")
-            }
-        } else if (opcao===2) {
-            let confOpc = confirm(`Sua opção de voto: ${opcao}\nCandidato: ${nome2}\n\nConfimar opção?\n\n(OK) para confimar voto\n(CANCELAR) para corrigir voto`) 
-            if (confOpc===true){  
-                candidato2 ++
-                console.log("Voto computado.")
-            }
-        } else if (opcao===3) {
-            let confOpc = confirm(`Sua opção de voto: ${opcao}\nCandidato: ${nome3}\n\nConfimar opção?\n\n(OK) para confimar voto\n(CANCELAR) para corrigir voto`) 
-            if (confOpc===true){  
-                candidato3 ++
-                console.log("Voto computado.")
-            }
-        } else if (opcao===5) {
-            let confOpc = confirm(`Sua opção de voto: ${opcao}\nVoto em Branco\n\nConfimar opção?\n\n(OK) para confimar voto\n(CANCELAR) para corrigir voto`) 
-            if (confOpc===true){  
-                votoBranco ++
-                console.log("Voto computado.")
-            }
-
-        /* senha/passo de segurança, onde o mesário insere o código "123456" para encerrar a votação daquela urna */
-        } else if (opcao===123456){ 
+        if (opcao === 123456) {
 
             /* passo extra de segurança, mais uma opção de confirmação para encerrar a votação. 
             * Destaque para o método toUpperCase, para caso o mesário digitar a confirmação em minúsculo, 
             transformar em maiúsculo e entrar na estrutura de decisão */
-            let seguranca = prompt("Você tem certeza que gostaria de encerrar a votação?\n\n(S) Para encerrar a votação\n(N) Para cancelar e voltar para o menu").charAt(0).toUpperCase() 
+            let seguranca = prompt("Você tem certeza que gostaria de encerrar a votação?\n\n(S) Para encerrar a votação\n(N) Para cancelar e voltar para o menu").charAt(0).toUpperCase()
 
             /* estrutura de decisão, caso o mesário tenha confirmado o encerramento da votação e passado pelos passos de segurança */
-            if(seguranca==="S"){
+            if (seguranca.charAt(0) === "S") {
                 console.clear()
 
                 /* soma de todos os votos */
-                votoTotal = candidato1 + candidato2 + candidato3 + votoBranco + votoNulo
-
-                /* mostrando os votos válidos, que é a soma dos votos de todos os candidatos */
-                console.log("Total de votos válidos:" , votoVálido = candidato1 + candidato2 + candidato3)
-
-                /* mostrando o total numerico e o percentual de votos de cada candidato, voto branco e nulo */
-                console.log(`\nCandidato: ${nome1}\nTotal de votos: ${candidato1}\nPercentual: ${((candidato1*100)/votoTotal).toFixed(2)}%`)
-                console.log(`\nCandidato: ${nome2}\nTotal de votos: ${candidato2}\nPercentual: ${((candidato2*100)/votoTotal).toFixed(2)}%`)
-                console.log(`\nCandidato: ${nome3}\nTotal de votos: ${candidato3}\nPercentual: ${((candidato3*100)/votoTotal).toFixed(2)}%`)
-                console.log(`\nTotal de votos em branco: ${votoBranco}\nPercentual: ${((votoBranco*100)/votoTotal).toFixed(2)}%`)
-                console.log(`\nTotal de votos nulo: ${votoNulo}\nPercentual ${((votoNulo*100)/votoTotal).toFixed(2)}%`)
-
-                /* estrutura de decisão para verificar o candidato ganhador */
-                if (candidato1 > candidato2 || candidato1 > candidato3){                  
-                    console.log(`\nCom um total de ${candidato1} voto(s) e um percentual de ${((candidato1*100)/votoTotal).toFixed(2)}%\nO vencedor das eleições é o candidato: ${nome1}`)
-                } else if (candidato2 > candidato1 || candidato2 > candidato3){
-                    console.log(`\nCom um total de ${candidato2} voto(s) e um percentual de ${((candidato2*100)/votoTotal).toFixed(2)}%, o vencedor das eleições é o candidato: ${nome2}`)
-                } else if (candidato3 > candidato2 || candidato3 > candidato2){
-                    console.log(`\nCom um total de ${candidato3} voto(s) e um percentual de ${((candidato3*100)/votoTotal).toFixed(2)}% o vencedor das eleições é o candidato: ${nome3}`)
-                } else if (votoVálido===0) { 
-                    console.log("\nNão houveram votos válidos suficientes para determinar o ganhador.")
-                } else {
-                    // caso tenha empate no numéro de votos de cada candidato
-                    console.log("\nHouve empate nas eleições! Não há um vencedor.") 
+                for (let i = 0; i < candidatos.length; i++) {
+                    votoTotal += candidatos[i][2]
+                    votoVálido += candidatos[i][2]
+                    if (i == candidatos.length - 1) {
+                        votoBranco = candidatos[i][2]
+                    }
                 }
 
-                //declarando a funcao na variavel para armazenar o horario do fim da votação
+                votoTotal += votoNulo
+
+                /* mostrando os votos válidos, que é a soma dos votos de todos os candidatos */
+                console.log("Total de votos válidos:", votoVálido.toString())
+
+                /* mostrando o total numerico e o percentual de votos de cada candidato, voto branco e nulo */
+                for (let i = 0; i < candidatos.length - 1; i++) {
+                    console.log(`\nCandidato: ${candidatos[i][1]} \nTotal de votos: ${candidatos[i][2]} \nPercentual: ${((candidatos[i][2] * 100) / votoTotal).toFixed(2)}% `)
+                    if (i == candidatos.length - 2) {
+                        console.log(`\nTotal de votos em Branco: ${candidatos[i + 1][2]} \nPercentual: ${((votoBranco * 100) / votoTotal).toFixed(2)}% `)
+                        console.log(`\nTotal de votos Nulo: ${votoNulo} \nPercentual ${((votoNulo * 100) / votoTotal).toFixed(2)}% `)
+                    }
+                }
+                for (let i = 0; i < candidatos.length - 1; i++) {
+                    if (votoVálido !== 0) {
+                        if (candidatos[i][2] > maiorVoto) {
+                            maiorVoto = candidatos[i][2]
+                            ganhador = `\nCom um total de ${candidatos[i][2]} voto(s) e um percentual de ${((candidatos[i][2] * 100) / votoTotal).toFixed(2)}%\nO vencedor das eleições é o candidato: ${candidatos[i][1]} \nNúmero eleitoral: ${candidatos[i][0]} `
+                        } else if (maiorVoto === candidatos[i][2]) {
+                            ganhador = "\nHouve empate nas eleições! Não há um vencedor."
+                        }
+                    } else {
+                        ganhador = "\nNão houveram votos válidos suficientes para determinar um vencedor."
+                        break;
+                    }
+                }
+
+                console.log(ganhador)
+
+                // declarando a funcao na variavel para armazenar o horario do fim da votação
                 horaFim = dataUrna();
-                break;                
+                break;
             }
 
-        // estrutura de decisão, caso o usuário não tenha inserido nenhuma opção de voto mostrada no console´, mostrando a opção de anular o voto
-        } else { 
-            // confirmação para anular o voto
-            let confNulo = confirm("Você gostaria de anular seu voto?"); 
-            if (confNulo == true){
-                votoNulo ++
-                console.log("Voto computado.")
+        } else {
+            for (let i = 0; i < candidatos.length; i++) {
+                if (candidatos[i] && candidatos[i][0] === opcao) {
+                    if (opcao === 5) {
+                        let confOpc = confirm(`Sua opção de voto: ${opcao} \nVoto em Branco\n\nConfimar opção?\n\n(OK) para confimar voto\n(CANCELAR) para corrigir voto`)
+                        if (confOpc === true) {
+                            candidatos[i][2]++
+                            console.log("Voto computado.")
+                            opcaoEscolhida = true
+                            break;
+                        }
+                    } else {
+                        let confOpc = confirm(`Sua opção de voto: ${opcao} \nCandidato: ${candidatos[i][1]} \n\nConfimar opção?\n\n(OK) para confimar voto\n(CANCELAR) para corrigir voto`)
+                        if (confOpc === true) {
+                            candidatos[i][2]++
+                            console.log("Voto computado.")
+                            opcaoEscolhida = true
+                            break;
+                        }
+                    }
+                }
+                else {
+                    opcaoEscolhida = false
+                }
             }
-        }   
-    } 
+            if (opcaoEscolhida === false) {
+                let confNulo = confirm("Você gostaria de anular seu voto?");
+                if (confNulo == true) {
+                    votoNulo++
+                    opcaoEscolhida = false
+                    console.log("Voto computado.")
+                }
+            }
+        }
+    }
 
     //chamando a variavel para mostrar a hora em que houve inicio da votação (foi armazenado no começo do código)
     console.log("\nVotação iniciada em:")
